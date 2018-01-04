adabb63638e47795a78e814687cc523306dd24f9
Dalliya <dalliyal@gmail.com>
2018-01-04 17:35:00 +0200

    added yet another function

--- Classwork_loops_04012018.py
+++ Classwork_loops_04012018.py
@@ -1,3 +1,14 @@
-for i in range(3):
-    print('hello world!', i)
-    print('happy new year!', i)
+for i in range(10):
+    if i % 2 == 0:
+        print('hello world!', i)
+    else:
+        print('happy new year!', i)
+
+s = 'abcabc'
+print(s[1:-1:2])
+for i in range(100, 20, -2):
+    print('Iteration#:', i)
+
+
+for i in range(10):
+    print('print smth!')
